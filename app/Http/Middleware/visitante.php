61cf7806734131bb43e25ea80adf0b6446e13044
DijaniraMuachifi <167861614+DijaniraMuachifi@users.noreply.github.com>
2025-09-22 00:16:38 -0300

    create middleware

---
+++ app/Http/Middleware/visitante.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class visitante
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if(Auth::user()->isVisitante)
+             return $next($request);
+        else{
+
+            alert(Auth::user()->name,'Sem autorização','info');
+
+            return redirect()->back();
+        }
+    }
+}
